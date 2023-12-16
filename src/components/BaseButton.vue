9975f20c46a0ae33f077f931fc51855b7c3191ff
S K Y <9327767-xxSkyy@users.noreply.gitlab.com>
2023-12-16 16:48:57 +0100

    feat: add counter

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="bg-neutral-700 px-2 py-1 rounded-md">
+    <slot />
+  </button>
+</template>
---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import BaseButton from "@/components/BaseButton.vue";
+import { ref } from "vue";
+
+const counter = ref(0);
+
+const increase = () => {
+  counter.value++;
+};
+
+const decrease = () => {
+  counter.value--;
+};
+</script>
+
+<template>
+  <div class="flex flex-col text-center gap-y-4">
+    <div class="text-2xl">Counter: {{ counter }}</div>
+    <div class="flex flex-row gap-x-4 justify-center">
+      <BaseButton @click="increase"> +1 </BaseButton>
+      <BaseButton @click="decrease">-1</BaseButton>
+    </div>
+  </div>
+</template>
